ba29ae2e23d1a5b3120f9dce17b7866ce8fabc9a
ASV2777 <104366730+ASV2777@users.noreply.github.com>
2022-04-25 23:22:32 +0530

    Add files via upload

---
+++ ques3(run) (1).py
@@ -0,0 +1,10 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\Abhiraj Virk\Desktop\python assignment 1\ques3.py ===
+Enter Student ID : 21102059
+Enter Student Name: Abhiraj Singh Virk
+Enter Gender(use values: 'F', 'M', 'U'(U for unknown)): F
+Enter Course Name: Btech Civil
+Enter CGPA: 8.5
+[21102059, 'Abhiraj Singh Virk', 'F', 'Btech Civil', 8.5]
---
+++ ques5(b).py
@@ -0,0 +1,5 @@
+color = ['Red', 'Green', 'White','Black','Pink', 'Yellow']
+color[3] = color[4] = 'Purple'
+print(color)
+
+
